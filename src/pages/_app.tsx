6a1a8d63532873d0cf608c9c0ef0e412f4bb32df
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2020-07-23 14:40:11 +0300

    Properly sets up Material-UI with Next.js
    So that it's compatible with Next.js's SSR/pre-rendering.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,17 +5,27 @@ import {
   responsiveFontSizes,
 } from "@material-ui/core/styles"
 import {AppProps} from "next/app"
+import {useEffect} from "react"
 
 import createTheme from "../theme/createTheme"
 
-const App = ({Component, pageProps}: AppProps): JSX.Element => (
-  <ThemeProvider
-    theme={createTheme(
-      responsiveFontSizes(createMuiTheme({palette: {type: "dark"}})),
-    )}>
-    <CssBaseline />
-    <Component {...pageProps} />
-  </ThemeProvider>
-)
+const App = ({Component, pageProps}: AppProps): JSX.Element => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side")
+    if (jssStyles != null) {
+      jssStyles.parentElement?.removeChild(jssStyles)
+    }
+  }, [])
+  return (
+    <ThemeProvider
+      theme={createTheme(
+        responsiveFontSizes(createMuiTheme({palette: {type: "dark"}})),
+      )}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
 
 export default App

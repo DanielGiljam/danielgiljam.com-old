c43aad13c22c1184ad4dcb7bfd4d5de6f4df7b17
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2020-07-24 21:43:08 +0300

    Adds src/pages/[project].tsx

---
+++ src/pages/[project].tsx
@@ -0,0 +1,36 @@
+import {promises as fs} from "fs"
+import path from "path"
+
+import {GetStaticPaths, GetStaticProps} from "next"
+
+import Project, {ProjectAsProp} from "../types/data/Project"
+
+const getProjects = async (): Promise<Project[]> =>
+  JSON.parse(
+    await fs.readFile(path.resolve("test/mock-data/projects.json"), "utf-8"),
+  ).projects
+
+export const getStaticProps: GetStaticProps<
+  ProjectAsProp,
+  {project?: string}
+> = async (context) => {
+  console.log(context)
+  const slug = context.params?.project ?? ""
+  const project = (await getProjects()).find((project) => project.slug === slug)
+  if (project == null)
+    throw new Error(
+      `Cannot get static props for project page "/${slug}". Project "${slug}" does not exist.`,
+    )
+  return {props: {project}}
+}
+
+export const getStaticPaths: GetStaticPaths = async (...args) => ({
+  paths: (await getProjects()).map((project) => "/" + project.slug),
+  fallback: false,
+})
+
+const ProjectPage = ({project}: ProjectAsProp): JSX.Element => (
+  <div>{project.name}</div>
+)
+
+export default ProjectPage
